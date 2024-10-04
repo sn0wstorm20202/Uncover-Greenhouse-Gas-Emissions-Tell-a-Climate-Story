b45958d516c0d965a9292289827319afae3337bc
reacfen <neitz@duck.com>
2024-10-04 18:09:26 +0530

    Update data

--- src/chloropleth-plot.js
+++ src/chloropleth-plot.js
@@ -1,3 +1,176 @@
+const countryMappedData = {
+    Afghanistan: "AFG",
+    Angola: "AGO",
+    Albania: "ALB",
+    "United Arab Emirates": "ARE",
+    Argentina: "ARG",
+    Armenia: "ARM",
+    Antarctica: "ATA",
+    Australia: "AUS",
+    Austria: "AUT",
+    Azerbaijan: "AZE",
+    Burundi: "BDI",
+    Belgium: "BEL",
+    Benin: "BEN",
+    "Burkina Faso": "BFA",
+    Bangladesh: "BGD",
+    Bulgaria: "BGR",
+    "The Bahamas": "BHS",
+    "Bosnia and Herzegovina": "BIH",
+    Belarus: "BLR",
+    Belize: "BLZ",
+    Bolivia: "BOL",
+    Brazil: "BRA",
+    Brunei: "BRN",
+    Bhutan: "BTN",
+    Botswana: "BWA",
+    "Central African Republic": "CAF",
+    Canada: "CAN",
+    Switzerland: "CHE",
+    Chile: "CHL",
+    China: "CHN",
+    Cameroon: "CMR",
+    "Democratic Republic of the Congo": "COD",
+    "Republic of the Congo": "COG",
+    Colombia: "COL",
+    "Costa Rica": "CRI",
+    Cuba: "CUB",
+    "Northern Cyprus": "CYP",
+    Cyprus: "CYP",
+    "Czech Republic": "CZE",
+    Germany: "DEU",
+    Djibouti: "DJI",
+    Denmark: "DNK",
+    "Dominican Republic": "DOM",
+    Algeria: "DZA",
+    Ecuador: "ECU",
+    Egypt: "EGY",
+    Eritrea: "ERI",
+    Spain: "ESP",
+    Estonia: "EST",
+    Ethiopia: "ETH",
+    Finland: "FIN",
+    Fiji: "FJI",
+    "Falkland Islands": "FLK",
+    France: "FRA",
+    Gabon: "GAB",
+    Georgia: "GEO",
+    Ghana: "GHA",
+    Guinea: "GIN",
+    Gambia: "GMB",
+    "Guinea Bissau": "GNB",
+    "Equatorial Guinea": "GNQ",
+    Greece: "GRC",
+    Greenland: "GRL",
+    Guatemala: "GTM",
+    Guyana: "GUY",
+    Honduras: "HND",
+    Croatia: "HRV",
+    Haiti: "HTI",
+    Hungary: "HUN",
+    Indonesia: "IDN",
+    India: "IND",
+    Ireland: "IRL",
+    Iran: "IRN",
+    Iraq: "IRQ",
+    Iceland: "ISL",
+    Israel: "ISR",
+    Italy: "ITA",
+    Jamaica: "JAM",
+    Jordan: "JOR",
+    Japan: "JPN",
+    Kazakhstan: "KAZ",
+    Kenya: "KEN",
+    Kyrgyzstan: "KGZ",
+    Cambodia: "KHM",
+    "South Korea": "KOR",
+    Kuwait: "KWT",
+    Laos: "LAO",
+    Lebanon: "LBN",
+    Liberia: "LBR",
+    Libya: "LBY",
+    "Sri Lanka": "LKA",
+    Lesotho: "LSO",
+    Lithuania: "LTU",
+    Luxembourg: "LUX",
+    Latvia: "LVA",
+    Morocco: "MAR",
+    Moldova: "MDA",
+    Madagascar: "MDG",
+    Mexico: "MEX",
+    Macedonia: "MKD",
+    Mali: "MLI",
+    Myanmar: "MMR",
+    Montenegro: "SCG",
+    Mongolia: "MNG",
+    Mozambique: "MOZ",
+    Mauritania: "MRT",
+    Malawi: "MWI",
+    Malaysia: "MYS",
+    Namibia: "NAM",
+    "New Caledonia": "NCL",
+    Niger: "NER",
+    Nigeria: "NGA",
+    Nicaragua: "NIC",
+    Netherlands: "NLD",
+    Norway: "NOR",
+    Nepal: "NPL",
+    "New Zealand": "NZL",
+    Oman: "OMN",
+    Pakistan: "PAK",
+    Panama: "PAN",
+    Peru: "PER",
+    Philippines: "PHL",
+    "Papua New Guinea": "PNG",
+    Poland: "POL",
+    "Puerto Rico": "PRI",
+    "North Korea": "PRK",
+    Portugal: "PRT",
+    Paraguay: "PRY",
+    Qatar: "QAT",
+    Romania: "ROU",
+    Russia: "RUS",
+    Rwanda: "RWA",
+    "Western Sahara": "ESH",
+    "Saudi Arabia": "SAU",
+    Sudan: "SDN",
+    "South Sudan": "SSD",
+    Senegal: "SEN",
+    "Solomon Islands": "SLB",
+    "Sierra Leone": "SLE",
+    "El Salvador": "SLV",
+    Somalia: "SOM",
+    "Republic of Serbia": "SCG",
+    Suriname: "SUR",
+    Slovakia: "SVK",
+    Slovenia: "SVN",
+    Sweden: "SWE",
+    Syria: "SYR",
+    Chad: "TCD",
+    Togo: "TGO",
+    Thailand: "THA",
+    Tajikistan: "TJK",
+    Turkmenistan: "TKM",
+    "East Timor": "TLS",
+    "Trinidad and Tobago": "TTO",
+    Tunisia: "TUN",
+    Turkey: "TUR",
+    Taiwan: "TWN",
+    "United Republic of Tanzania": "TZA",
+    Uganda: "UGA",
+    Ukraine: "UKR",
+    Uruguay: "URY",
+    USA: "USA",
+    Uzbekistan: "UZB",
+    Venezuela: "VEN",
+    Vietnam: "VNM",
+    Vanuatu: "VUT",
+    Yemen: "YEM",
+    "South Africa": "ZAF",
+    Zambia: "ZMB",
+    Zimbabwe: "ZWE",
+};
+
 // The svg
 const svg = d3.select("#chloropleth-map"),
     width = +svg.attr("width"),
@@ -38,14 +211,23 @@ Promise.all([
         d3.selectAll(".Country")
             .transition()
             .duration(150)
-            .style("opacity", 0.6).style("stroke-width", "1px");
+            .style("opacity", 0.6)
+            .style("stroke-width", "1px");
         // .style("stroke", "transparent");
-        d3.select(this).transition().duration(100).style("opacity", 1).style("stroke-width", "2px");
+        d3.select(this)
+            .transition()
+            .duration(100)
+            .style("opacity", 1)
+            .style("stroke-width", "2px");
         // .style("stroke", "black");
     };
 
     let mouseLeave = function (_) {
-        d3.selectAll(".Country").transition().duration(150).style("opacity", 0.8).style("stroke-width", "1px");
+        d3.selectAll(".Country")
+            .transition()
+            .duration(150)
+            .style("opacity", 0.8)
+            .style("stroke-width", "1px");
         // .style("stroke", "transparent");
         // d3.select(this)
         //     .transition()
@@ -54,7 +236,10 @@ Promise.all([
     };
 
     let mouseClick = function (_, d) {
-        console.log(d.properties.name, d.total);
+        const countryCode = countryMappedData[d.properties.name];
+        if (countryCode) {
+            console.log(d.properties.name, d.total, countryCode);
+        }
     };
 
     // Draw the map
